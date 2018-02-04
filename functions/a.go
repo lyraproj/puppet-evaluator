9d0d3a47e33ef3f8ee52ec1c12fad0b28471199b
Thomas Hallgren <thomas@tada.se>
2018-02-05 00:18:38 +0100

    Add extra "side effects" import of impl package

--- functions/a.go
+++ functions/a.go
@@ -1,5 +1,8 @@
 package functions
 
+// Ensure that impl package is initialized
+import _ "github.com/puppetlabs/go-evaluator/impl"
+
 // Ensure that loader package is initialized
 import _ "github.com/puppetlabs/go-evaluator/loader"
 
