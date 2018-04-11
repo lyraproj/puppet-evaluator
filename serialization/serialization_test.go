b84ab3fa0cb45858efbbcc7636d493eb01d9c284
Thomas Hallgren <thomas@tada.se>
2018-04-11 10:16:47 +0200

    Fix serializer examples after changing Do and Process method signatures

--- serialization/serialization_test.go
+++ serialization/serialization_test.go
@@ -15,9 +15,9 @@ func TestRichDataRoundtrip(t *testing.T) {
 	ver, _ := semver.NewVersion(1, 0, 0)
 	v := types.WrapSemVer(ver)
 	buf := bytes.NewBufferString(``)
-	v2 := eval.Puppet.Produce(func(ctx eval.Context) eval.PValue {
+	v2, _ := eval.Puppet.Produce(func(ctx eval.Context) (eval.PValue, error) {
 		DataToJson(ctx, NewToDataConverter(ctx, types.SingletonHash2(`rich_data`, types.Boolean_TRUE)).Convert(v), buf, eval.EMPTY_MAP)
-		return NewFromDataConverter(ctx, eval.EMPTY_MAP).Convert(JsonToData(ctx, ``, buf))
+		return NewFromDataConverter(ctx, eval.EMPTY_MAP).Convert(JsonToData(ctx, ``, buf)), nil
 	})
 	if !eval.Equals(v, v2) {
 		t.Errorf(`Expected %T '%s', got %T '%s'`, v, v, v2, v2)
@@ -52,8 +52,8 @@ func ExampleJsonToData() {
 
 func ExampleFromDataConverter_Convert() {
 	data := types.WrapHash4(map[string]interface{}{`__pcore_type__`: `SemVer`, `__pcore_value__`: `1.0.0`})
-	ver := eval.Puppet.Produce(func(ctx eval.Context) eval.PValue {
-		return NewFromDataConverter(ctx, eval.EMPTY_MAP).Convert(data)
+	ver, _ := eval.Puppet.Produce(func(ctx eval.Context) (eval.PValue, error) {
+		return NewFromDataConverter(ctx, eval.EMPTY_MAP).Convert(data), nil
 	})
 	fmt.Printf("%T\n", ver)
 	fmt.Println(ver)
