ac0c68cd6b4b482a38b421c940c3a09577edce9e
Thomas Hallgren <thomas@tada.se>
2018-04-11 11:11:56 +0200

    Fix failing pcore/pcore_test

--- pcore/pcore_test.go
+++ pcore/pcore_test.go
@@ -6,11 +6,12 @@ import (
 )
 
 func TestPcore(t *testing.T) {
-	eval.Puppet.Do(func(ctx eval.Context) {
+	eval.Puppet.Do(func(ctx eval.Context) error {
 		l, _ := eval.Load(ctx, eval.NewTypedName(eval.TYPE, `Pcore::ObjectTypeExtensionType`))
 		x, ok := l.(eval.PType)
 		if !(ok && x.Name() == `Pcore::ObjectTypeExtensionType`) {
 			t.Errorf(`failed to load %s`, `Pcore::ObjectTypeExtensionType`)
 		}
+		return nil
 	})
 }
