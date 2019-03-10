3a23ef04320a3ea6dae284f0c5e9b791b44d02be
Thomas Hallgren <thomas@tada.se>
2019-03-10 10:19:49 +0100

    Move ErrorObject and impl to servicesdk

--- pdsl/eval.go
+++ pdsl/eval.go
@@ -19,28 +19,3 @@ type Evaluator interface {
 type ParserExtension interface {
 	Evaluate(e Evaluator) px.Value
 }
-
-type ErrorObject interface {
-	px.PuppetObject
-
-	// Kind returns the error kind
-	Kind() string
-
-	// Message returns the error message
-	Message() string
-
-	// IssueCode returns the issue code
-	IssueCode() string
-
-	// PartialResult returns the optional partial result. It returns
-	// pcore.UNDEF if no partial result exists
-	PartialResult() px.Value
-
-	// Details returns the optional details. It returns
-	// an empty map when o details exist
-	Details() px.OrderedMap
-}
-
-var ErrorFromReported func(c px.Context, err issue.Reported) ErrorObject
-
-var NewError func(c px.Context, message, kind, issueCode string, partialResult px.Value, details px.OrderedMap) ErrorObject
